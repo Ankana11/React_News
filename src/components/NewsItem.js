c41724f745ef8cb4aac446733c2052e248c73afc
Ankana <reach2ankana@gmail.com>
2024-11-20 20:43:57 +0530

    setup

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+       newsitem 
+      </div>
+    )
+  }
+}
+
+export default NewsItem
