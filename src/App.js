3e2828dcb77fe1da6b9c4bf9ce7d89159c1caff8
Ankana <reach2ankana@gmail.com>
2024-11-21 18:40:36 +0530

    use state in class

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
-import NewsItem from './components/NewsItem';
+// import NewsItem from './components/NewsItem';
 
 export default class App extends Component {
   render() {
@@ -11,7 +11,7 @@ export default class App extends Component {
       <div>
        <Navbar/>
        <News/>
-       <NewsItem/>
+      
       </div>
     )
   }
