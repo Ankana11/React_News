bff0ae86b0c808bf7e1433a43f48df2498d11c56
Ankana <reach2ankana@gmail.com>
2024-11-23 21:47:43 +0530

    fix

--- src/components/News.js
+++ src/components/News.js
@@ -224,6 +224,30 @@ async componentDidMount(){
   this.setState({articles: persedata.articles})
 }
 
+handlenext= async()=>{
+console.log("click on next");
+let url= `https://newsapi.org/v2/top-headlines?country=us&apiKey=e57231eade29412a843937296b240cb2&page=${this.state.page + 1}`
+let data = await fetch(url)
+let persedata = await data.json()
+console.log(persedata);
+this.setState({
+page: this.state.page + 1,
+articles: persedata.articles
+
+})
+}
+
+handleprev= async()=>{
+  console.log("click on next");
+  let url= `https://newsapi.org/v2/top-headlines?country=us&apiKey=e57231eade29412a843937296b240cb2&page=${this.state.page - 1}`
+  let data = await fetch(url)
+  let persedata = await data.json()
+  console.log(persedata);
+  this.setState({
+  page: this.state.page - 1,
+  articles: persedata.articles
+  })
+}
   render() {
     return (
       <div className='container my-3'>
@@ -234,13 +258,13 @@ async componentDidMount(){
  return <div className="col-md-4 my-2" key={element.url}>
   <NewsItem   title={element.title ? element.title.slice(0, 60) : "No title available"} description={element.description ? element.description.slice(0, 100) : "No description available"}  imageurl={!element.urlToImage ? "https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fd1e00ek4ebabms.cloudfront.net%2Fproduction%2Fc95a2e5d-7819-4ee2-9baf-eda69718903b.jpg?source=next-barrier-page" : element.urlToImage}newsurl={element.url} />
   </div>
-})}
-  
- 
- 
-  
-     
+
+})} 
       </div>
+     <div className="container d-flex justify-content-between my-3">
+     <button disabled={this.state.page<=1} type="button"  className="btn btn-dark" onClick={this.handleprev}>&larr;Previous</button>
+     <button type="button"  className="btn btn-dark" onClick={this.handlenext}>Next&rarr;</button>
+     </div>
       </div>
     )
   }
